a54001957a6447e75ff1dc33cfde2acf5127fc81
1nv8rZim <msf9542@rit.edu>
2024-02-06 20:30:37 -0500

    Add notification_type parameter to PublishNotification function

--- pkg/cache/notification.go
+++ pkg/cache/notification.go
@@ -2,8 +2,10 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/scorify/backend/pkg/graph/model"
 )
 
 const (
@@ -11,8 +13,16 @@ const (
 	globalNotification = "global_notification"
 )
 
-func (c *Cache) PublishNotification(ctx context.Context, message string) *redis.IntCmd {
-	return c.Client.Publish(context.Background(), globalNotification, message)
+func (c *Cache) PublishNotification(ctx context.Context, message string, notification_type model.NotificationType) (*redis.IntCmd, error) {
+	out, err := json.Marshal(model.Notification{
+		Message: message,
+		Type:    notification_type,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Client.Publish(context.Background(), globalNotification, out), nil
 }
 
 func (c *Cache) SubscribeNotification(ctx context.Context) *redis.PubSub {
