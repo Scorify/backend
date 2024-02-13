0db1755c7760c7acf302ed140952aded3ee1a0e1
1nv8rZim <msf9542@rit.edu>
2024-02-12 20:37:08 -0500

    Remove error handling for `cmd.Execute()`

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"github.com/scorify/backend/pkg/cmd"
-	"github.com/sirupsen/logrus"
 )
 
 //go:generate go run -mod=mod github.com/scorify/generate
 
 func main() {
-	err := cmd.Execute()
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to execute command")
-	}
+	cmd.Execute()
 }
