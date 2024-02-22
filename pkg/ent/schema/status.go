bdf725076046f90427eeee15a32aacff4379710e
1nv8rZim <msf9542@rit.edu>
2024-02-21 21:49:06 -0500

    Implement Status Schema

--- pkg/ent/schema/status.go
+++ pkg/ent/schema/status.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
 
 // Status holds the schema definition for the Status entity.
 type Status struct {
@@ -9,10 +14,47 @@ type Status struct {
 
 // Fields of the Status.
 func (Status) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			StructTag(`json:"id"`).
+			Comment("The uuid of a status").
+			Unique().
+			Immutable().
+			Default(uuid.New),
+		field.String("error").
+			StructTag(`json:"error"`).
+			Comment("The error message of the status").
+			Optional(),
+		field.Enum("status").
+			StructTag(`json:"status"`).
+			Comment("The status of the status").
+			Values("up", "down", "unknown").
+			Default("unknown"),
+		field.Time("updated_at").
+			StructTag(`json:"updated_at"`).
+			Comment("The update time of the status").
+			Optional(),
+		field.Int("weight"),
+	}
 }
 
 // Edges of the Status.
 func (Status) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("check", Check.Type).
+			StructTag(`json:"check"`).
+			Comment("The check of a status").
+			Required().
+			Unique(),
+		edge.To("round", Round.Type).
+			StructTag(`json:"round"`).
+			Comment("The round of a status").
+			Required().
+			Unique(),
+		edge.To("user", User.Type).
+			StructTag(`json:"user"`).
+			Comment("The user of a status").
+			Required().
+			Unique(),
+	}
 }
