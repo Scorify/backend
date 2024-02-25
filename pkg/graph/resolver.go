c3b3820e38b39c7271b0921c343e45511763cdef
1nv8rZim <msf9542@rit.edu>
2024-02-24 19:43:16 -0500

    Update Resolver struct in resolver.go

--- pkg/graph/resolver.go
+++ pkg/graph/resolver.go
@@ -3,6 +3,7 @@ package graph
 import (
 	"github.com/scorify/backend/pkg/cache"
 	"github.com/scorify/backend/pkg/ent"
+	"github.com/scorify/backend/pkg/structs"
 )
 
 // This file will not be regenerated automatically.
@@ -12,6 +13,8 @@ import (
 //go:generate go run github.com/99designs/gqlgen generate
 
 type Resolver struct {
-	Ent   *ent.Client
-	Redis *cache.Cache
+	Ent     *ent.Client
+	Redis   *cache.Cache
+	Checks  chan *ent.Status
+	Minions *structs.Counter
 }
