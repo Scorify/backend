968962d878e5ace3f5f3412f1208a7878d65f5e7
1nv8rZim <msf9542@rit.edu>
2024-02-26 22:03:15 -0500

    Add logging unaryInterceptor to gRPC server

--- pkg/grpc/server/main.go
+++ pkg/grpc/server/main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"time"
 
 	"github.com/scorify/backend/pkg/config"
 	"github.com/scorify/backend/pkg/grpc/proto"
+	"github.com/scorify/backend/pkg/structs"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
@@ -14,6 +16,7 @@ import (
 type minionServer_s struct {
 	proto.UnimplementedMinionServiceServer
 
+	counter            *structs.Counter
 	ScoreTasks         <-chan *proto.GetScoreTaskResponse
 	ScoreTaskResponses chan<- *proto.SubmitScoreTaskRequest
 }
@@ -23,6 +26,35 @@ var (
 	minionServer *minionServer_s
 )
 
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	start := time.Now()
+
+	var minionID string
+	switch req := req.(type) {
+	case *proto.GetScoreTaskRequest:
+		minionServer.counter.Increment()
+		defer minionServer.counter.Decrement()
+		minionID = req.MinionId
+	case *proto.SubmitScoreTaskRequest:
+		minionID = req.MinionId
+	case *proto.HeartbeatRequest:
+		minionID = req.MinionId
+	default:
+		minionID = "unknown"
+	}
+
+	resp, err := handler(ctx, req)
+
+	logrus.WithFields(logrus.Fields{
+		"method":   info.FullMethod,
+		"took":     time.Since(start),
+		"minions":  minionServer.counter.Get(),
+		"minionID": minionID,
+	}).Info("gRPC request")
+
+	return resp, err
+}
+
 func Serve(ctx context.Context, scoreTaskChan <-chan *proto.GetScoreTaskResponse, scoreTaskReponseChan chan<- *proto.SubmitScoreTaskRequest) {
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", config.GRPC.Host, config.GRPC.Port))
 	if err != nil {
@@ -31,16 +63,19 @@ func Serve(ctx context.Context, scoreTaskChan <-chan *proto.GetScoreTaskResponse
 
 	// TODO: Implement TLS Configuration
 
-	grpcServer = grpc.NewServer()
+	grpcServer = grpc.NewServer(
+		grpc.UnaryInterceptor(unaryInterceptor),
+	)
 
 	minionServer = &minionServer_s{
 		ScoreTasks:         scoreTaskChan,
 		ScoreTaskResponses: scoreTaskReponseChan,
+		counter:            structs.NewCounter(),
 	}
 
 	proto.RegisterMinionServiceServer(grpcServer, minionServer)
 
-	logrus.Infof("gRPC server listening on %s:%d", config.GRPC.Host, config.GRPC.Port)
+	logrus.Infof("Starting gRPC server on %s:%d", config.GRPC.Host, config.GRPC.Port)
 
 	go func() {
 		<-ctx.Done()
