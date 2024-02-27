abc4a798b074c9302e6bb7f4aa1ec45d63afcbc0
1nv8rZim <msf9542@rit.edu>
2024-02-26 22:02:48 -0500

    Remove logging statements from gRPC server files

--- pkg/grpc/server/getScoreTask.go
+++ pkg/grpc/server/getScoreTask.go
@@ -4,11 +4,8 @@ import (
 	"context"
 
 	"github.com/scorify/backend/pkg/grpc/proto"
-	"github.com/sirupsen/logrus"
 )
 
 func (s *minionServer_s) GetScoreTask(ctx context.Context, req *proto.GetScoreTaskRequest) (*proto.GetScoreTaskResponse, error) {
-	logrus.WithField("minion_id", req.GetMinionId()).Info("received get score task request")
-
 	return <-s.ScoreTasks, nil
 }
--- pkg/grpc/server/heartbeat.go
+++ pkg/grpc/server/heartbeat.go
@@ -4,11 +4,8 @@ import (
 	"context"
 
 	"github.com/scorify/backend/pkg/grpc/proto"
-	"github.com/sirupsen/logrus"
 )
 
 func (*minionServer_s) Heartbeat(ctx context.Context, req *proto.HeartbeatRequest) (*proto.HeartbeatResponse, error) {
-	logrus.WithField("minion_id", req.GetMinionId()).Info("received heartbeat")
-
 	return &proto.HeartbeatResponse{}, nil
 }
