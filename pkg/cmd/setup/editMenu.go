c7f9a5ab697b06616def97dbbe86f07700a2913d
1nv8rZim <msf9542@rit.edu>
2024-03-14 22:27:11 -0400

    Refactor editMenu.go to improve item editing

--- pkg/cmd/setup/editMenu.go
+++ pkg/cmd/setup/editMenu.go
@@ -7,14 +7,15 @@ import (
 )
 
 type item struct {
-	label   string
-	value   string
-	editing bool
+	label string
+	value string
 }
 
 type editModel struct {
-	cursor int
-	items  []item
+	itemCursor int
+	editCursor int
+	items      []item
+	editting   bool
 }
 
 func newEditModel() editModel {
@@ -35,20 +36,52 @@ func (m editModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "up":
-			if m.cursor > 0 {
-				m.cursor--
+			if !m.editting {
+				m.itemCursor = max(m.itemCursor-1, 0)
 			}
 		case "down":
-			if m.cursor < len(m.items)-1 {
-				m.cursor++
+			if !m.editting {
+				m.itemCursor = min(m.itemCursor+1, len(m.items)-1)
+			}
+		case "left":
+			if m.editting {
+				m.editCursor = max(m.editCursor-1, 0)
+			}
+		case "right":
+			if m.editting {
+				m.editCursor = min(m.editCursor+1, len(m.items[m.itemCursor].value))
 			}
 		case "enter":
-			m.items[m.cursor].editing = !m.items[m.cursor].editing
+			m.editting = !m.editting
+			m.editCursor = len(m.items[m.itemCursor].value)
 		case "ctrl+c":
 			return m, tea.Quit
+		case "backspace", "delete":
+			if m.editting {
+				if m.editCursor < len(m.items[m.itemCursor].value) {
+					m.items[m.itemCursor].value = m.items[m.itemCursor].value[:m.editCursor-1] + m.items[m.itemCursor].value[m.editCursor:]
+				} else if m.editCursor == len(m.items[m.itemCursor].value) {
+					m.items[m.itemCursor].value = m.items[m.itemCursor].value[:m.editCursor-1]
+					m.editCursor--
+				}
+			}
 		default:
-			if m.items[m.cursor].editing {
-				m.items[m.cursor].value += string(msg.Runes)
+			if m.editting {
+				m.items[m.itemCursor].value += string(msg.Runes)
+				m.editCursor++
+			} else {
+				switch msg.String() {
+				case "j":
+					m.itemCursor = min(m.itemCursor+1, len(m.items)-1)
+				case "k":
+					m.itemCursor = max(m.itemCursor-1, 0)
+				case "h":
+					m.editCursor = max(m.editCursor-1, 0)
+				case "l":
+					m.editCursor = min(m.editCursor+1, len(m.items[m.itemCursor].value))
+				case "q":
+					return m, tea.Quit
+				}
 			}
 		}
 	}
@@ -58,14 +91,19 @@ func (m editModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m editModel) View() string {
 	s := ""
 	for i, item := range m.items {
-		prefix := "  "
-		if i == m.cursor {
-			prefix = "> "
+		prefix := "[ ]"
+		if i == m.itemCursor {
+			if m.editting {
+				prefix = "[*]"
+			} else {
+				prefix = "[>]"
+			}
 		}
-		if item.editing {
-			s += fmt.Sprintf("%s*%s*: %s (editing)\n", prefix, item.label, item.value)
+
+		if m.editting && i == m.itemCursor {
+			s += fmt.Sprintf("%s %s: %s\n", prefix, item.label, item.value[:m.editCursor]+"|"+item.value[m.editCursor:])
 		} else {
-			s += fmt.Sprintf("%s%s: %s\n", prefix, item.label, item.value)
+			s += fmt.Sprintf("%s %s: %s\n", prefix, item.label, item.value)
 		}
 	}
 	return s
