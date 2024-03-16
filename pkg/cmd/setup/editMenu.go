0a5a4fdb09cd1e3730c5533cb3566c0bb91059c9
1nv8rZim <msf9542@rit.edu>
2024-03-15 20:44:10 -0400

    Use real config values in editMenu

--- pkg/cmd/setup/editMenu.go
+++ pkg/cmd/setup/editMenu.go
@@ -2,14 +2,17 @@ package setup
 
 import (
 	"fmt"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/fatih/color"
+	"github.com/scorify/backend/pkg/config"
 )
 
 type item struct {
-	label string
-	value string
+	label   string
+	value   string
+	private bool
 }
 
 type editModel struct {
@@ -24,8 +27,22 @@ var cursorFmt = color.New(color.FgBlack, color.BgWhite).SprintFunc()
 func newEditModel() editModel {
 	return editModel{
 		items: []item{
-			{label: "Name", value: "John Doe"},
-			{label: "Email", value: "john.doe@example.com"},
+			{label: "Domain", value: config.Domain},
+			{label: "Port", value: fmt.Sprintf("%d", config.Port)},
+			{label: "Interval", value: config.IntervalStr},
+			{label: "JWT Timeout", value: config.JWT.TimeoutStr},
+			{label: "JWT Secret", value: config.JWT.Secret, private: true},
+			{label: "Postgres Host", value: config.Postgres.Host},
+			{label: "Postgres Port", value: fmt.Sprintf("%d", config.Postgres.Port)},
+			{label: "Postgres User", value: config.Postgres.User},
+			{label: "Postgres Password", value: config.Postgres.Password, private: true},
+			{label: "Postgres DB", value: config.Postgres.DB},
+			{label: "Redis Host", value: config.Redis.Host},
+			{label: "Redis Port", value: fmt.Sprintf("%d", config.Redis.Port)},
+			{label: "Redis Password", value: config.Redis.Password, private: true},
+			{label: "GRPC Host", value: config.GRPC.Host},
+			{label: "GRPC Port", value: fmt.Sprintf("%d", config.GRPC.Port)},
+			{label: "GRPC Secret", value: config.GRPC.Secret, private: true},
 		},
 	}
 }
@@ -114,7 +131,11 @@ func (m editModel) View() string {
 				s += fmt.Sprintf("%s %s: %s\n", prefix, item.label, item.value[:m.editCursor]+cursorFmt(string(item.value[m.editCursor]))+item.value[m.editCursor+1:])
 			}
 		} else {
-			s += fmt.Sprintf("%s %s: %s\n", prefix, item.label, item.value)
+			if item.private {
+				s += fmt.Sprintf("%s %s: %s\n", prefix, item.label, strings.Repeat("*", len(item.value)))
+			} else {
+				s += fmt.Sprintf("%s %s: %s\n", prefix, item.label, item.value)
+			}
 		}
 	}
 	return s
