f3cfcd8de93db17b3d2c240ac488a4d7fe5ee16a
1nv8rZim <msf9542@rit.edu>
2024-03-24 22:09:24 -0400

    Update scoreboard pub/sub channels

--- pkg/cache/scoreboard_update.go
+++ pkg/cache/scoreboard_update.go
@@ -9,19 +9,51 @@ import (
 )
 
 const (
-	// ScoreboardUpdateChannel is the key for the scoreboard update pub/sub in redis
-	ScoreboardUpdateChannel = "scoreboard_update_channel"
+	// ScoreboardRoundUpdateChannel is the channel to publish scoreboard round updates
+	ScoreboardRoundUpdateChannel = "scoreboard_round_update_channel"
+
+	// ScoreboardStatusUpdateChannel is the channel to publish scoreboard status updates
+	ScoreboardStatusUpdateChannel = "scoreboard_status_update_channel"
+
+	// ScoreboardScoreUpdateChannel is the channel to publish scoreboard score updates
+	ScoreboardScoreUpdateChannel = "scoreboard_score_update_channel"
 )
 
-func PublishScoreboardUpdate(ctx context.Context, redisClient *redis.Client, scoreboardUpdate *model.ScoreboardUpdate) (*redis.IntCmd, error) {
-	out, err := json.Marshal(scoreboardUpdate)
+func PublishScoreboardRoundUpdate(ctx context.Context, redisClient *redis.Client, roundUpdate *model.RoundUpdateScoreboard) (*redis.IntCmd, error) {
+	out, err := json.Marshal(roundUpdate)
 	if err != nil {
 		return nil, err
 	}
 
-	return redisClient.Publish(ctx, ScoreboardUpdateChannel, out), nil
+	return redisClient.Publish(ctx, ScoreboardRoundUpdateChannel, out), nil
 }
 
-func SubscribeScoreboardUpdate(ctx context.Context, redisClient *redis.Client) *redis.PubSub {
-	return redisClient.Subscribe(ctx, ScoreboardUpdateChannel)
+func SubscribeScoreboardRoundUpdate(ctx context.Context, redisClient *redis.Client) *redis.PubSub {
+	return redisClient.Subscribe(ctx, ScoreboardRoundUpdateChannel)
+}
+
+func PublishScoreboardStatusUpdate(ctx context.Context, redisClient *redis.Client, statusUpdate *model.StatusUpdateScoreboard) (*redis.IntCmd, error) {
+	out, err := json.Marshal(statusUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	return redisClient.Publish(ctx, ScoreboardStatusUpdateChannel, out), nil
+}
+
+func SubscribeScoreboardStatusUpdate(ctx context.Context, redisClient *redis.Client) *redis.PubSub {
+	return redisClient.Subscribe(ctx, ScoreboardStatusUpdateChannel)
+}
+
+func PublishScoreboardScoreUpdate(ctx context.Context, redisClient *redis.Client, scoreUpdate *model.ScoreUpdateScoreboard) (*redis.IntCmd, error) {
+	out, err := json.Marshal(scoreUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	return redisClient.Publish(ctx, ScoreboardScoreUpdateChannel, out), nil
+}
+
+func SubscribeScoreboardScoreUpdate(ctx context.Context, redisClient *redis.Client) *redis.PubSub {
+	return redisClient.Subscribe(ctx, ScoreboardScoreUpdateChannel)
 }
