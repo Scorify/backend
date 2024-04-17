e57e57fb23eba42cbbb5318b42eda0ee489bc00f
1nv8rZim <msf9542@rit.edu>
2024-04-16 21:08:50 -0400

    Add object key functions and constants

--- pkg/cache/objects.go
+++ pkg/cache/objects.go
@@ -5,10 +5,39 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
 type ObjectKey string
+
+const (
+	ScoreboardObjectKey ObjectKey = "object-scoreboard"
+)
+
+func GetCheckObjectKey(checkID uuid.UUID) ObjectKey {
+	return ObjectKey("object-check-" + checkID.String())
+}
+
+func GetCheckConfigObjectKey(checkConfigID uuid.UUID) ObjectKey {
+	return ObjectKey("object-check-config-" + checkConfigID.String())
+}
+
+func GetRoundObjectKey(roundID uuid.UUID) ObjectKey {
+	return ObjectKey("object-round-" + roundID.String())
+}
+
+func GetScoreCacheObjectKey(roundID uuid.UUID) ObjectKey {
+	return ObjectKey("object-score-cache-" + roundID.String())
+}
+
+func GetStatusObjectKey(statusID uuid.UUID) ObjectKey {
+	return ObjectKey("object-status-" + statusID.String())
+}
+
+func GetUserObjectKey(userID uuid.UUID) ObjectKey {
+	return ObjectKey("object-user-" + userID.String())
+}
 
 func SetObject(ctx context.Context, redisClient *redis.Client, key ObjectKey, obj interface{}, expiration time.Duration) error {
 	out, err := json.Marshal(obj)
