8f75df825e41f2b48dbbed836e0bce263b6c0255
1nv8rZim <msf9542@rit.edu>
2024-05-03 22:05:39 -0400

    Refactor File struct to use FileType enum in pkg/structs/file.go

--- pkg/structs/file.go
+++ pkg/structs/file.go
@@ -6,11 +6,18 @@ import (
 	"github.com/google/uuid"
 )
 
+type FileType string
+
+const (
+	FileTypeInject     FileType = "inject"
+	FileTypeSubmission FileType = "submission"
+)
+
 type File struct {
 	ID   uuid.UUID `json:"id"`
 	Name string    `json:"name"`
 }
 
-func (file *File) Path(fileType string, parentID uuid.UUID) string {
-	return filepath.Join("/api/files/", fileType, parentID.String(), file.ID.String(), file.Name)
+func (file *File) Path(fileType FileType, parentID uuid.UUID) string {
+	return filepath.Join("/api/files/", string(fileType), parentID.String(), file.ID.String(), file.Name)
 }
