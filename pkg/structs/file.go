2ee90c08c770c87b851dedd44aa66ca86d9033a2
1nv8rZim <msf9542@rit.edu>
2024-05-04 21:57:16 -0400

    Add WriteFile method to File struct in pkg/structs/file.go

--- pkg/structs/file.go
+++ pkg/structs/file.go
@@ -1,6 +1,8 @@
 package structs
 
 import (
+	"io"
+	"os"
 	"path/filepath"
 
 	"github.com/google/uuid"
@@ -20,4 +22,21 @@ type File struct {
 
 func (file *File) Path(fileType FileType, parentID uuid.UUID) string {
 	return filepath.Join("/api/files/", string(fileType), parentID.String(), file.ID.String(), file.Name)
+}
+
+func (file *File) WriteFile(fileType FileType, parentID uuid.UUID, reader io.ReadSeeker) error {
+	filePath := file.Path(fileType, parentID)
+
+	err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	fileHandle, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(fileHandle, reader)
+	return err
 }
