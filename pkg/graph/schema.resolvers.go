b05912d39cff45e0a17755be218c3873c298bbf8
1nv8rZim <msf9542@rit.edu>
2024-05-05 21:53:27 -0400

    Refactor Inject resolver to use model.File struct for file representation in schema.resolvers.go

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -108,14 +108,18 @@ func (r *configResolver) User(ctx context.Context, obj *ent.CheckConfig) (*ent.U
 
 // Files is the resolver for the files field.
 func (r *injectResolver) Files(ctx context.Context, obj *ent.Inject) ([]*model.File, error) {
-	var err error
-
-	files := make([]string, len(obj.Files))
+	files := make([]*model.File, len(obj.Files))
 
 	for i, file := range obj.Files {
-		files[i], err = file.APIPath(structs.FileTypeInject, obj.ID)
+		url, err := file.APIPath(structs.FileTypeInject, obj.ID)
 		if err != nil {
 			logrus.Errorf("failed to get file path: %v", err)
+		}
+
+		files[i] = &model.File{
+			ID:   file.ID,
+			Name: file.Name,
+			URL:  url,
 		}
 
 	}
