28fc647e76c9ee815abd40891947de078e1082f2
1nv8rZim <msf9542@rit.edu>
2024-05-18 12:17:45 -0400

    Add RubricTemplate and RubricTemplateField structs
    This commit adds the RubricTemplate and RubricTemplateField structs to the project. These structs are used to represent rubric templates and their fields, including the name and maximum score. This addition will enable the implementation of rubric template functionality in the application.

--- pkg/structs/rubric.go
+++ pkg/structs/rubric.go
@@ -1,5 +1,15 @@
 package structs
 
+type RubricTemplateField struct {
+	Name     string `json:"name"`
+	MaxScore int    `json:"max_score"`
+}
+
+type RubricTemplate struct {
+	Fields   []RubricTemplateField `json:"fields"`
+	MaxScore int                   `json:"max_score"`
+}
+
 type RubricField struct {
 	Name     string `json:"name"`
 	Score    int    `json:"score"`
