6e7d30da1ee1e8b2b4fae9194dffab1cb83defb3
1nv8rZim <msf9542@rit.edu>
2024-05-19 20:21:07 -0400

    Properly inherit use of rubric template

--- pkg/structs/rubric.go
+++ pkg/structs/rubric.go
@@ -11,15 +11,12 @@ type RubricTemplate struct {
 }
 
 type RubricField struct {
-	Name     string `json:"name"`
-	Score    int    `json:"score"`
-	MaxScore int    `json:"max_score"`
-	Notes    string `json:"notes"`
+	Name  string `json:"name"`
+	Score int    `json:"score"`
+	Notes string `json:"notes"`
 }
 
 type Rubric struct {
-	Fields   []RubricField `json:"fields"`
-	Score    int           `json:"score"`
-	MaxScore int           `json:"max_score"`
-	Notes    string        `json:"notes"`
+	Fields []RubricField `json:"fields"`
+	Notes  string        `json:"notes"`
 }
